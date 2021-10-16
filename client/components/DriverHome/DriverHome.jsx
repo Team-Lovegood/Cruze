481ee868fc55a0eb5b67c12cd94e9fb35bf5ee4b
Jordan Villacorta <jvillacortadla@gmail.com>
2021-10-16 17:30:23 -0400

    Fix Views with a <>, import Map and remove DriverMao.

--- client/components/DriverHome/DriverHome.jsx
+++ client/components/DriverHome/DriverHome.jsx
@@ -2,24 +2,28 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import RiderList from './RiderList.jsx';
 import DriverMap from './DriverMap.jsx';
+import Map from './Map.jsx';
 
 const DriverHome = () => {
   return (
-    <View style={styles.landingContainer}>
-      <DriverMap />
-      <Text style={styles.title}>
-        Hello, Tim
-      </Text>
-      <View>
-        <RiderList />
+    <>
+      <View style={styles.landingContainer}>
+        {/* <DriverMap /> */}
+        <Map />
       </View>
-    </View>
+      <View style={styles.landingContainer}>
+        <Text style={styles.title}>
+          Hello, Tim
+        </Text>
+          <RiderList />
+        </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   landingContainer: {
-    // color: "#20232a"
+    flex: 1
   },
   title: {
     marginTop: 16,
