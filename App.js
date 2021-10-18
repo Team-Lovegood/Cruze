ce09336b096daae12a8edb319d0f68691fa556b0
Jin Cheng <jincheng411@gmail.com>
2021-10-18 14:45:35 -0400

    sovled merge confict

--- App.js
+++ App.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
-import LandingPage from './client/components/Login/LandingPage.jsx';
+// import LandingPage from './client/components/Login/LandingPage.jsx';
 import RiderProfile from './client/components/Profiles/RiderProfile.jsx';
 import DriverProfile from './client/components/Profiles/DriverProfile.jsx';
 import DriverHome from './client/components/DriverHome/DriverHome.jsx';
-=======
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { LoginScreen, SignupScreen, RiderProfileScreen, DriverHomeScreen, DriverProfileScreen } from './Screens.js';
->>>>>>> 5d34c3fa97d32cd254e8825d3a27a00946444ea0
 //import riderhome
 //import driverhome
 
-const AuthStack = createStackNavigator();
-const DriverStack = createStackNavigator();
+// const AuthStack = createStackNavigator();
+// const DriverStack = createStackNavigator();
 
 export default function App() {
   return (
     <>
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 
     {/* <View style={styles.container}>
@@ -35,47 +27,11 @@ export default function App() {
   </View>*/}
 
 
-    <View style={styles.container}>
+    {/* <View style={styles.container}>
       <DriverHome />
       <Text>Driver Home</Text>
       <StatusBar style="auto" />
-    </View>
-=======
-      <View style={styles.container}>
-=======
-      {/* <View style={styles.container}>
->>>>>>> 59d5632ab1da2906fca736873e3f3a32cc2b97f7
-        <Text>Team Lovegood</Text>
-        <StatusBar style="auto" />
-      </View> */}
-
-      {/* <DriverProfileScreen/> */}
-      {/* <RiderProfileScreen/> */}
-
-      <NavigationContainer>
-        <AuthStack.Navigator>
-          <AuthStack.Screen name='Login' component={LoginScreen} options={{headerShown: false}}/>
-          <AuthStack.Screen name='Signup' component={SignupScreen} options={{headerShown: false}}/>
-          <AuthStack.Screen name='DriverProfile' component={DriverProfileScreen} options={{headerShown: false}}/>
-          <AuthStack.Screen name='RiderProfile' component={RiderProfileScreen} options={{headerShown: false}}/>
-        </AuthStack.Navigator>
-      </NavigationContainer>
-
-      {/* <View style={styles.container}>
-        <Text>Profiles</Text>
-        <StatusBar style="auto" />
-      </View>
-
-      <View style={styles.container}>
-        <Text>Rider Home</Text>
-        <StatusBar style="auto" />
-      </View>
-
-      <View style={styles.container}>
-        <Text>Driver Home</Text>
-        <StatusBar style="auto" />
-      </View> */}
->>>>>>> 5d34c3fa97d32cd254e8825d3a27a00946444ea0
+    </View> */}
     </>
   );
 }
