047c0305a348d53117a0d4d20f6265d812a74769
EndlessDavidDu <trickyheight830@gmail.com>
2021-10-20 21:21:52 -0400

    delete unnecessary file

--- server/languages/routes.js
+++
@@ -1,6 +0,0 @@
-const LanguageController = require("./languages.controller");
-const router = require("express").Router();
-
-router.get("/:language", LanguageController.getLanguagePackages);
-
-module.exports = router;
