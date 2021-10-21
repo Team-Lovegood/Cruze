d900fce67acdc1160de8a13bcba9f4d241bca7cb
EndlessDavidDu <trickyheight830@gmail.com>
2021-10-20 21:20:32 -0400

    solve conflict and change ip address

--- App.js
+++ App.js
@@ -52,14 +52,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-<<<<<<< HEAD
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-=======
     backgroundColor: '#fff',
     // alignItems: 'center',
     // justifyContent: 'center',
->>>>>>> origin
   },
 });
