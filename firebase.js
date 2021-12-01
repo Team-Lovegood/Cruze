65e7e8fc0ee1ad0d66495a7b8b8687ada26494d8
Jordan Villacorta <jvillacortadla@gmail.com>
2021-12-01 12:50:00 -0700

    Remove commented out code and comments.

--- firebase.js
+++ firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import * as firebase from "firebase";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD11JG_Aqimp6drwsazs-kGNjOl0T1ffoo",
   authDomain: "cruze-auth.firebaseapp.com",
@@ -13,7 +9,6 @@ const firebaseConfig = {
   appId: "1:804047206744:web:7d6fab795b9a6428454d2d",
 };
 
-// Initialize Firebase
 let app;
 if (firebase && firebase.apps && firebase.apps.length === 0) {
   app = firebase.initializeApp(firebaseConfig);
